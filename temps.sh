0300a6f13e3adbb63167c452a144319eb58b7a16
cristiandiiorio <112891194+cristiandiiorio@users.noreply.github.com>
2022-09-05 20:48:21 +0200

    Update temps.sh

--- temps.sh
+++ temps.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/bash
-#!/usr/bin/python3.9
 
-#acquisisce temperatura ed elimina lettere
-temp=$(vcgencmd measure_temp  | egrep -o '[0-9]*\.[0-9]*')
-#converte temperatura in intero invece di float
+#gets the temperature, then cleans the output to make sure it's only made of numbers
+temp=$( vcgencmd measure_temp  | egrep -o '[0-9]*\.[0-9]*' )
+
+#float to integer conversion (needed due to -gt command in line 15)
 temp=${temp%.*}
 
+#echoes date
 echo $(date)
+#echoes temperature
 echo $temp
 
-#condizione critica
+#critical temperature (60 degrees celsius by default)
 if [ "$temp" -gt "60" ]
-then #invia mail
+then #email is sent
 	python3 hmail.py
 
 fi
